26c6a5d2ea4a7ea0107821df95e81a1495c4d2d3
Sunny Master <master.codeworks@gmail.com>
2024-08-19 08:17:53 -0400

    add status message on the home-page

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const router = Router()
 // GET localhost:3000/
 router.get('/', function(req, res) {
   res.render('index', {
-    title: 'Inventory Master'
+    title: 'Inventory Master',
+    status: "Welcome to Inventory Master! Sign up or Sign in to use this app or just explore as a guest! "
   })
 })
 
